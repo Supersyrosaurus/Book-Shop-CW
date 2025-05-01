d44953b091422afcd836830a491640096b2c062e
Supersyrosaurus <118223878+Supersyrosaurus@users.noreply.github.com>
2025-05-01 23:02:58 +0100

    Added responsive nav bar

--- js/index.js
+++ js/index.js
@@ -4,3 +4,13 @@ window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0)
 })
+
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
