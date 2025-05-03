d7bd3349c5fd7ad5eb5038c7d72b3d758bf99ff7
Supersyrosaurus <118223878+Supersyrosaurus@users.noreply.github.com>
2025-05-03 23:28:01 +0100

    Working on js for pages

--- js/pay.js
+++ js/pay.js
@@ -1,4 +1,19 @@
 
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0)
+})
+
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
+
 document.getElementById("send").addEventListener("click", (e)=>) {
     e.preventdefault()
     //get input 5%
