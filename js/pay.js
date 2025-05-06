a632ea5aa5fb42043f72a92eb9a2bcfbcaab5267
Supersyrosaurus <118223878+Supersyrosaurus@users.noreply.github.com>
2025-05-06 22:46:52 +0100

    Working on Pay js

--- js/pay.js
+++ js/pay.js
@@ -14,28 +14,54 @@ function hideSidebar() {
     sidebar.style.display = 'none'
 }
 
-document.getElementById("send").addEventListener("click", (e)=>) {
-    e.preventdefault()
+document.getElementById("send").addEventListener("click", e)
+
+function f() {
+    // alert('this is woking')
+    console.error('this is working')
+}
+
+function e() {
+    
+    alert('working')
+    // e.preventdefault()
     //get input 5%
-    const CC = document.getElementById("CC").value
+    alert('working2')
+    const CC = document.getElementById("cc").value
     const month = document.getElementById("month").value
+    const year = document.getElementById("year").value
+    const CVV = document.getElementById('cvv').value
     //validate user input 10%
-    const patt = /^5[1-5][0-9]{14}$/
+    const CCpatt = /^5[1-5][0-9]{14}$/
     //psuedo: if CC !match pattern
+    const CVVpatt = /^[0-9]{3,4}$/
+    
+    if (CC.match(CCpatt) == null)
     {
         alert("errorMsg(Your Credit Card Number is Wrong)")
-        location.reload()
+        // location.reload()
 
     }
-    
+
     //do same with CVV, exp date
-    // create a today date (today = ...) 
-    todayM = today.getMonth()
-    todayY = today.getYear()
+    if (CVV.match(CVVpatt) == null) 
+    {
+        alert("errorMsg(Your Security Code Number is Wrong)")
+    }
+
+    // create a today date (today = ...)
+    today = new Date()
+    todayM = today.getMonth() + 1
+    todayY = today.getFullYear()
+    // alert(String(todayM))
+    // alert(String(todayY))
     //if today month is greater and today year is greater than exp date then card expired
+    if ((todayY > year) || ((todayM > month) && (todayY == year)))
     {
         alert("errorMsg(Your Credit Card is Expired)")
     }
+
+    
     //send to API 15%
     //lec 9 pp
     //fetch
